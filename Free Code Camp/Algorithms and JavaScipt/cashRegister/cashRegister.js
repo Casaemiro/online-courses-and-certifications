9d08114690a6a60e736cb53c7c62f0a2365c6a3e
Akoneh Silas <silasmagho18@gmail.com>
2022-05-14 07:48:01 +0100

    version4

--- Free Code Camp/Algorithms and JavaScipt/cashRegister/cashRegister.js
+++ Free Code Camp/Algorithms and JavaScipt/cashRegister/cashRegister.js
@@ -42,8 +42,67 @@
 //STEPS//________________________________________________________________________________________
 
 
+function checkCashRegister(price, cash, cid) {
+    let coinsandBillsArray = [
+        ["PENNY", 1.01],
+        ["NICKEL", 2.05],
+        ["DIME", 3.1],
+        ["QUARTER", 4.25],
+        ["ONE", 90],
+        ["FIVE", 55],
+        ["TEN", 20],
+        ["TWENTY", 60],
+        ["ONE HUNDRED", 100]
+        ]
+        let coinValue = [0.01,0.05,0.1,0.25,1,5,10,20,100]
+    let finalObject = { "status" : "",change : []};
+    let change1 = cash - price;
+    let changeArr = [];
+    let tempMoney;
+    sumCashInDrawer = cid.reduce((sum,elem) => +sum + +elem[1],0)
+    console.log(sumCashInDrawer)
+    console.log(change1)
+    
+    if(sumCashInDrawer < change1 ){
+        finalObject["status"] = "INSUFFICIENT_FUNDS"
+        finalObject["change"] = [];
+        console.log(finalObject)
+    }else if(sumCashInDrawer == change1 ){
+        finalObject["status"] = "CLOSED"
+        finalObject["change"] = [...cid];
+        console.log(finalObject)
+    }else{
+        for(let i = coinsandBillsArray.length - 1; i >= 0 ; i--){
+            //console.log(coinsandBillsArray[i][1])
+            if(change1 >= coinsandBillsArray[i][1]){
+                tempMoney = change1 - (Math.floor(change1/coinValue[i]))*coinValue[i]
+                change1 = tempMoney
+                coinsandBillsArray[i][1] -= (Math.floor(change1/coinValue[i]))*coinValue[i]
+            }
+           finalObject["change"].push(coinsandBillsArray[i][1])
 
+        } finalObject["status"] = "OPEN"
+             
+            console.log(finalObject)
+           
+    }
+
+    return finalObject;
+  }
+  
 
 
 
 //TESTS//________________________________________________________________________________________
+
+// checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) //should return an object.
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) //should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) //should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) //should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
