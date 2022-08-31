dd41f82b55d37295d26f3dd268cdc729e8a5b483
Casaemiro <silasmagho@gmail.com>
2022-08-31 09:27:29 +0100

    codeWars

--- CodeWars/Snail/snail.js
+++ CodeWars/Snail/snail.js
@@ -1,6 +1,6 @@
 //Codewars 4kyu
 //__________________________________________________________________________________________________________________________
-// Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+// Given an n x n array, return finalArray the array elements arranged from outermost elements to the middle element, traveling clockwise.
 
 // array = [[1,2,3],
 //          [4,5,6],
@@ -25,9 +25,92 @@
 
 
 snail = function (array) {
-    let finalArray = array
-    console.log(array);
+    lengthOfArray = array[0].length
+    let arrr = []
+    for (let i = 0; i < array.length; i++) {
+        arrr.push(...array)
+        
+    }
+    let lengthOfTotalArray = arrr.length
+    console.log(lengthOfTotalArray);
+    let finalArray = []
+    let startRow = 0;
+    let lastElementIndex = lengthOfArray - 1;
+    let lastRowIndex = array.length - 1;
+    let rowVarry = 0;
+
+
+
+    for (let i = startRow; i < array.length; i++) {
+        if(lengthOfTotalArray == 1){
+    console.log("finalArray")
+    finalArray.push(...array[i])
+    console.log(finalArray)
+
+            return finalArray
+        }
+        console.log(i);
+        console.log(array);
+        
+        if (i == 0) {
+            finalArray.push(...array[i])
+        }
+        if (i > 0 && i < lastRowIndex) {
+            finalArray.push(array[i][lastElementIndex])
+            console.log(finalArray);
+        }
+        if (i == lastRowIndex) {
+            finalArray.push(...array[i].reverse())
+            if (finalArray.length == lengthOfTotalArray) {
+    console.log("finalArray")
+    console.log(finalArray)
+                return finalArray
+            }
+            if(array[i].length > 1) {
+                for (let j = lastRowIndex - 1; j > startRow; j--) {
+                finalArray.push(array[j][0])
+                console.log("array");
+                console.log(finalArray);
+                if (finalArray.length == lengthOfTotalArray) {
+    console.log("finalArray")
+    console.log(finalArray)
+                    return finalArray
+                }
+            }
+            }
+            
+            array.pop()
+            array.shift()
+
+            for (let j = 0; j < array.length; j++) {
+                array[j].pop()
+                array[j].shift()
+                console.log(array);
+                console.log(finalArray);
+            }
+            i = -1
+            startRow = 0;
+
+            lastElementIndex -= 2;
+            lastRowIndex = array.length - 1;
+            if (finalArray.length == lengthOfTotalArray) {
+    console.log("finalArray")
+    console.log(finalArray)
+                return finalArray
+            }
+        }
+        console.log(finalArray)
+    }
+
+
+    console.log(finalArray)
+    console.log("finalArray")
 }
-snail([[1, 2, 3],
-       [8, 9, 4],
-       [7, 6, 5]])
+// snail([[1, 2, 3],
+//     [8, 9, 4],
+//     [7, 6, 5]])
+    snail([[]])
+    //  snail([[1, 2, 3],
+    //     [8, 9, 4],
+    //     [8, 9, 4],
+    //     [7, 6, 5]])
---
+++ Free Code Camp/Front end Libraries/quotes.js
@@ -0,0 +1,38 @@
+
+
+
+export const quote = [
+    
+    {
+        text:"This is the first quote",
+        author:"Silas",
+    },
+    {
+        text:"This is the second quote",
+        author:"Boris",
+    },
+    {
+        text:"This is the third quote",
+        author:"Nervis",
+    },
+    {
+        text:"This is the fourth quote",
+        author:"Neville",
+    },
+    {
+        text:"This is the fifth quote",
+        author:"Glory",
+    },
+    {
+        text:"This is the sixth quote",
+        author:"Mercy",
+    },
+    {
+        text:"This is the seventh quote",
+        author:"David",
+    },
+    {
+        text:"This is the eigth quote",
+        author:"Johnson",
+    },
+]
